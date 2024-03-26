7708a855b8bee81766cd723c09749939382d5c52
Taro Serigano <taroserigano@gmail.com>
2024-03-26 11:53:31 -0500

    Update s3.js

--- image-upload-s3-master/backend/s3.js
+++ image-upload-s3-master/backend/s3.js
@@ -7,6 +7,7 @@ const region = process.env.AWS_BUCKET_REGION
 const accessKeyId = process.env.AWS_ACCESS_KEY
 const secretAccessKey = process.env.AWS_SECRET_KEY
 
+// create the s3 Object 
 const s3 = new S3({
   region,
   accessKeyId,
@@ -15,14 +16,17 @@ const s3 = new S3({
 
 // uploads a file to s3
 function uploadFile(file) {
-  const fileStream = fs.createReadStream(file.path)
 
+  // take the file into the file system 
+  const fileStream = fs.createReadStream(file.path)
+  
   const uploadParams = {
     Bucket: bucketName,
     Body: fileStream,
     Key: file.filename
   }
 
+  // upload to s3 
   return s3.upload(uploadParams).promise()
 }
 exports.uploadFile = uploadFile
